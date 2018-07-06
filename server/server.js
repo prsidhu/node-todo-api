4d7fa29c1ae2f6176871e39d3fb2ce517edd041f
Preet Sidhu <preetsidhu.bits@gmail.com>
2018-07-06 14:19:59 +0530

    GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var bodyParser = require('body-parser')
+const {ObjectID} = require('mongodb')
 
 
 var mongoose = require('./db/mongoose')
@@ -25,6 +26,23 @@ app.get('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(e)
     })
+})
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send({
+            error: 'Invalid id'
+        })
+    } 
+    Todo.findById(id).then((todo) => {
+        if(todo)
+            return res.status(200).send({todo})
+        else
+            return res.status(404).send({
+                error: 'no todo found for the given id'
+            })
+    }, (e) => res.status(400).send(e))
 })
 
 app.listen(3000, () => {
