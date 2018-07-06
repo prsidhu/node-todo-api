f2cb511e3dce077efda94158cd6f159cf5e920f8
Preet Sidhu <preetsidhu.bits@gmail.com>
2018-07-06 13:54:28 +0530

    test /GET route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,8 +5,19 @@ const {app} = require('./../server')
 const {Todo} = require('./../models/todo')
 
 
+const todos = [
+    {
+        text: 'test 1'
+    },
+    {
+        text: 'text 2'
+    }
+]
+
 beforeEach((done) => {
-    Todo.remove({}).then(() => done())
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos)
+    }).then(() => done())
 })
 
 describe('POST /todos', () => {
@@ -26,8 +37,8 @@ describe('POST /todos', () => {
                 }
 
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(1)
-                    expect(todos[0].text).toBe(text)
+                    expect(todos.length).toBe(3)
+                    expect(todos[2].text).toBe(text)
                     done()
                 }).catch((e) => done(e))
             })
@@ -48,9 +59,21 @@ describe('POST /todos', () => {
                     return done(err)
 
                 Todo.find({}).then((todos) => {
-                    expect(todos.length).toBe(0)
+                    expect(todos.length).toBe(2)
                     done()
                 }).catch((e) => done(e))
             })
+    })
+})
+
+describe('GET /todos', () => {
+    it('should get all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2)
+            })
+            .end(done)
     })
 })
