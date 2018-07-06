180115c2951fcbc74d74ec154a625165dc966bca
Preet Sidhu <preetsidhu.bits@gmail.com>
2018-07-06 13:46:36 +0530

    add GET /todos route

--- server/server.js
+++ server/server.js
@@ -19,6 +19,14 @@ app.post('/todos', (req, res) => {
     }, (e) => res.status(400).send(e))
 })
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos})
+    }, (e) => {
+        res.status(400).send(e)
+    })
+})
+
 app.listen(3000, () => {
     console.log(`running on 3000`)
 })
