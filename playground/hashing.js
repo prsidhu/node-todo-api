80b4a7cc1cde5e23f61c278069565b056d049e8d
Preet Sidhu <preetsidhu.bits@gmail.com>
2018-07-07 20:50:29 +0530

    GET /users/me route

---
+++ playground/hashing.js
@@ -0,0 +1,8 @@
+const {SHA256} = require('crypto-js')
+const jwt = require('jsonwebtoken')
+
+var data = {id: 10}
+
+var token = jwt.sign(data, '123abc')
+
+
--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const _ = require('lodash')
 var mongoose = require('./db/mongoose')
 var {Todo} = require('./models/todo')
 var {User} = require('./models/user')
+var {authenticate} = require('./middleware/authenticate')
 
 var app = express()
 const port = process.env.PORT || 3000
@@ -87,6 +88,21 @@ app.patch('/todos/:id', (req, res) => {
 
         return res.status(200).send({todo})
     }).catch((e) => res.status(400).send())
+})
+
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password'])
+    var user = new User(body)
+
+    user.save().then(() => {
+        return user.generateAuthToken()
+    }).then((token) => {
+        res.header('x-auth', token).send(user)
+    }).catch((e) => res.status(400).send(e))
+})
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user)
 })
 
 app.listen(port, () => {
