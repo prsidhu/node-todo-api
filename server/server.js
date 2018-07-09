905af310f86ac0c0f653e679eadeceab90d13c9d
Preet Sidhu <preetsidhu.bits@gmail.com>
2018-07-09 14:32:24 +0530

    DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -116,6 +116,12 @@ app.post('/users/login', (req, res) => {
     }).catch((e) => res.status(400).send(e))
 })
 
+app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send()
+    }, () => res.status(400).send())
+})
+
 app.listen(port, () => {
     console.log(`running on ${port}`)
 })
